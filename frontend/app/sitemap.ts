ba3a195b3ddf3d0d2086bde2ea5efebc04d65173
Big Dreams Web3 - Crafts ◯/| <123673781+bigdreamsweb3@users.noreply.github.com>
2026-05-01 23:31:04 +0100

    Implement sitemap function for metadata routes
    Add sitemap function to generate metadata for routes.

--- frontend/app/sitemap.ts
+++ frontend/app/sitemap.ts
@@ -1,1 +1,19 @@
 
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://trustlink-pay.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {/* {
+      url: 'https://vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    }, */}
+  ]
+}
